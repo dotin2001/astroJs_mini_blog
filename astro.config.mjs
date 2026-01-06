f43f6aa1e158697c47ec4fb17fa5ed4759fab269
nitod2001 <dotinvta2@gmail.com>
2026-01-06 09:11:52 +0700

    update set up base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "node:path";
+
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("src"),
+      },
+    },
+  }
+});
